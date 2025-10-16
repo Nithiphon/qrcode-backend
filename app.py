151c15b0b0eb54700dcedd6a2a51132f8e78aa6f
nithiphon <nithiphon1712@gmail.com>
2025-10-16 23:25:54 +0700

    add

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-# backend/app.py
+# app.py
 from flask import Flask, request, send_file
 from flask_cors import CORS
 import qrcode
 import io
 
 app = Flask(__name__)
-CORS(app)  # อนุญาตให้เว็บอื่นเรียก API ได้
+CORS(app)
 
 @app.route('/generate', methods=['POST'])
 def generate_qr():
     try:
         data = request.json.get('text', '').strip()
+        fg_color = request.json.get('fg', '#000000')  # สีจุด QR
+        bg_color = request.json.get('bg', '#ffffff')  # สีพื้นหลัง
+
         if not data:
             return {"error": "กรุณากรอกข้อความหรือลิงก์!"}, 400
 
-        # สร้าง QR Code
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -23,9 +25,8 @@ def generate_qr():
         )
         qr.add_data(data)
         qr.make(fit=True)
-        img = qr.make_image(fill_color="black", back_color="white")
+        img = qr.make_image(fill_color=fg_color, back_color=bg_color)
 
-        # ส่งรูปเป็น response
         img_io = io.BytesIO()
         img.save(img_io, 'PNG')
         img_io.seek(0)
@@ -35,7 +36,6 @@ def generate_qr():
     except Exception as e:
         return {"error": "เกิดข้อผิดพลาด: " + str(e)}, 500
 
-# สำหรับ Render.com
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
